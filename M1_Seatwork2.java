8b0634714f5acb8beba54856e9557e1352c94f8d
Marriela A. Senoran <masenoran@headoffice.corp.bpi.com.ph>
2025-12-04 16:16:51 +0800

    initial commit

---
+++ M1_Seatwork2.java
@@ -0,0 +1,10 @@
+package com.bpi.javatraining.main;
+
+public class M1_Seatwork2 {
+
+	public static void main(String[] args) {
+		WhiteBoard wb = new WhiteBoard();
+		int sum = wb.addNumbers(5,3);
+		System.out.println("The total is: " + String.valueOf(sum));
+	}
+}
---
+++ WhiteBoard.java
@@ -0,0 +1,8 @@
+package com.bpi.javatraining.main;
+
+public class WhiteBoard {
+	int addNumbers(int addend1,int addend2) {
+		int sum = addend1 + addend2;
+		return sum;
+	}
+}
