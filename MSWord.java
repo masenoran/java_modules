65122057148ce5e95504ea7925c6efa399f72250
Marriela A. Senoran <masenoran@headoffice.corp.bpi.com.ph>
2025-12-10 09:42:10 +0800

    Initial commit

---
+++ MSWord.java
@@ -0,0 +1,18 @@
+package M2_Activity5;
+
+public class MSWord extends Program {
+	
+	public MSWord(String name) {
+		this.setName(name);
+	}
+	
+	@Override
+	public void run() {
+		System.out.println("Opening MS Word...");		
+	}
+	
+	@Override
+	public void stop() {
+		System.out.println("Stopping MS Word...");
+	}
+}
